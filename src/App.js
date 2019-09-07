b9d56dd4530c38a4f27442f8cf65955017f182e3
jiachihuynh <jaja.huynh@gmail.com>
2019-09-07 23:48:45 +0700

    Create todolist component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todolist from './components/todolist';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello Jiachi!
+       <Todolist title="Hello Jiachi!"/>
       </div>
     );
   }
-  
 }
 
