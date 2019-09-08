111aae702dd7600c8df0570d15831b98f32a6259
jiachihuynh <jaja.huynh@gmail.com>
2019-09-08 23:52:50 +0700

    test props name

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 export default class Todolist extends Component {
     render() {
         return (
-            <div>{this.props.title}</div>
-            <h2>{this.props.name}</h2>
+            <div>
+              <div>{this.props.title}</div>  
+              <h2>{this.props.name}</h2>
+            </div>
         );
     }
 }
