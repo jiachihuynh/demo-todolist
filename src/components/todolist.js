a02c3f888cab14f9ec5137d14e65c59d1a264706
jiachihuynh <jaja.huynh@gmail.com>
2019-09-08 23:50:15 +0700

    test props

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -4,6 +4,7 @@ export default class Todolist extends Component {
     render() {
         return (
             <div>{this.props.title}</div>
+            <h2>{this.props.name}</h2>
         );
     }
 }
