05c80b6f17425e262b0f26e550b920a4d5471b58
jiachihuynh <jaja.huynh@gmail.com>
2019-10-01 20:55:12 +0700

    Demo-todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todolist from './components/todolist';
+import checkImg from '../img/check.svg';
+import checkCompleteImg from '../img/check-complete.svg';
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.todoItems = [
-      { title: 'Watching TV', isComplete: true },
-      { title: 'Go to cinema', isComplete: false },
-      { title: 'Reading book', isComplete: true }
-    ];
+  constructor(props) {
+    super(props);
+    this.state = { 
+      todoItems:  [
+        { title: 'Watching TV', isComplete: true },
+        { title: 'Go to cinema', isComplete: false },
+        { title: 'Reading book', isComplete: true }
+      ]
+    };
   }
+
+  onItemClicked (item) {
+      const {todoItems} = this.state;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0,index),
+          {
+            ...item,
+            isComplete: !item.isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      })
+    }
+
+
   render() {
-    return (
-      <div className="App">
-        {
-          this.todoItems.map((item,index) => 
-          <Todolist key={index} item={item} />)
-        }
-      </div>
-    );
-  }
+    const {todoItems} = this.state;
+      return (
+        <div className="App">
+          {todoItems.length && todoItems.map((item,index) => 
+            <Todolist 
+              key={index} 
+              item={item} 
+              onClick={() => this.onItemClicked(item)} />)
+          }
+        </div>
+      );
+    }
 }
 
--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import './todolist.css';
+import classNames from 'classnames';
+import checkImg from '../img/check.svg';
+import checkCompleteImg from '../img/check-complete.svg';
 
 export default class Todolist extends Component {
     render() {
-      const { item } = this.props;
-      let className = "TodoList";
-      if (item.isComplete) {
-          className += " TodoList-complete";
+      const { item, onClick } = this.props;
+      let url = checkImg;
+      if (item.isComplete){
+          url = checkCompleteImg ;
       }
       return (
-          <div className={className}>
-              <div>{this.props.item.title}</div>  
+          <div onClick={onClick} 
+               className={classNames('TodoList', {
+                   "TodoList-complete" : item.isComplete
+          })}>
+              <img src={url} width={32} heght={32} />
+              <p>{item.title}</p>  
           </div>
     );
     }
